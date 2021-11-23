7e70503617061f538c2b90e9a5a8ab033c6f6854
Jorge Noda <jknoda@hotmail.com>
2021-11-23 15:58:41 -0300

    novo banco yamazakiapi

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,24 @@
 module.exports = {
+    // nsystemapi
+    // dialect: 'mysql',
+    // host: 'us-cdbr-east-04.cleardb.com',
+    // timezone: "-03:00",
+    // username: 'bc466a0a0f6f3c',
+    // password: '24cb7c0b',
+    // database: 'heroku_af970ad18f26c07',
+    // define: {
+    //     timestamps: false
+    // }
+
+    //yamazakijudoapi
     dialect: 'mysql',
     host: 'us-cdbr-east-04.cleardb.com',
     timezone: "-03:00",
-    username: 'bc466a0a0f6f3c',
-    password: '24cb7c0b',
-    database: 'heroku_af970ad18f26c07',
+    username: 'b7a0a0d9997a24',
+    password: '57c8ca9b',
+    database: 'heroku_75966b25f38e6d4',
     define: {
         timestamps: false
     }
+
 }
